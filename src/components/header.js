0e5226a58cc089243e6ab2d1d87866b549565666
Yacoub Gadban <yacoub.gadban@gmail.com>
2021-12-31 23:50:16 +0200

    ko

--- src/components/header.js
+++ src/components/header.js
@@ -4,16 +4,47 @@ class Header extends React.Component{
 
     render(){
             return(
-                <div className="container">
-                <ul className="nav" style={{backgroundColor: '#e3f2fd'}}>
-                <li><a href="/" ><i className="fa fa-home"></i>Home</a></li>
-                <li><a href="/about"><i className="fa fa-user"></i>About</a></li>
-                <li><a href="/projects"><i className="fa fa-list"></i>Projects</a></li>
-                <li><a href="/contact"><i className="fa fa-briefcase"></i>Contact</a></li>
-              </ul>
-              <hr/>
-              
-              </div>
+            <div className="header">
+               <div class="container">
+               <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+  <Link to="/">
+    <a class="navbar-brand" href="/">Home</a>
+    </Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarText">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      
+      <Link to="/about">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page">About</a>
+        </li>
+        </Link>
+        
+        <Link to="/Projects">
+        <li class="nav-item">
+          <a class="nav-link" >Projects</a>
+        </li>
+        </Link>
+        <Link to="/contact">
+        <li class="nav-item">
+          <a class="nav-link" >Contact</a>
+        </li>
+        </Link>
+      </ul>
+      <span class="navbar-text">
+        Navbar text with an inline element
+      </span>
+    </div>
+  </div>
+</nav>
+                    </div>
+                    
+            <hr/>
+           
+            </div>
             )
         }
 
@@ -21,4 +52,4 @@ class Header extends React.Component{
 
 
 
-export default Header; 
+export default Header;
