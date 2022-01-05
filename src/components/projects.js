6a097aec57bb9cb3387d23a559a4ca3d25018a17
Yacoub Gadban <yacoub.gadban@gmail.com>
2022-01-06 01:57:24 +0200

    style

--- src/components/projects.js
+++ src/components/projects.js
@@ -15,15 +15,15 @@ class Projects extends React.Component{
   <div className="row">
     <div className="col-sm">
         <img className="project-img"  src="https://media.istockphoto.com/photos/and-a-concept-yellow-question-mark-glowing-amid-black-question-marks-picture-id1305169776?b=1&k=20&m=1305169776&s=170667a&w=0&h=mpYdh2MzGN_rqxoRNlO5KWGCCq3ZktzSfp-wA0nD9no=" alt=""/>    
-    <p className="name">project 1</p>
+    <p className="desc">project 1</p>
     </div>
     <div className="col-sm">
     <img className="project-img" src="https://media.istockphoto.com/photos/and-a-concept-yellow-question-mark-glowing-amid-black-question-marks-picture-id1305169776?b=1&k=20&m=1305169776&s=170667a&w=0&h=mpYdh2MzGN_rqxoRNlO5KWGCCq3ZktzSfp-wA0nD9no=" alt=""/>    
-    <p className="name">project 2</p>
+    <p className="desc">project 2</p>
     </div>
     <div className="col-sm">
     <img className="project-img" src="https://media.istockphoto.com/photos/and-a-concept-yellow-question-mark-glowing-amid-black-question-marks-picture-id1305169776?b=1&k=20&m=1305169776&s=170667a&w=0&h=mpYdh2MzGN_rqxoRNlO5KWGCCq3ZktzSfp-wA0nD9no=" alt=""/>    
-    <p className="name">project 3</p>
+    <p className="desc">project 3</p>
     </div>
 
 
